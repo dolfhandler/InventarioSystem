ca640c3f31343b2e768805e47d36f58992bb945b
dolfhandler <dolfhandler@gmail.com>
2020-11-17 20:44:01 -0500

    rjdlc changes

---
+++ Inventario/Inventario/Controllers/InventoryController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Inventario.Context;
+using Inventario.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventario.Controllers {
+    public class InventoryController : Controller {
+        public InventoryContext db { get; set; }
+
+        public InventoryController() {
+            db = new InventoryContext();
+        }
+
+        public IActionResult Inventory() {
+            var stock = db.Inventory.ToList();
+            LoadProducts();
+            return View(stock);
+        }
+        
+        [HttpPost]
+        public IActionResult Input(InventoryModel model) {
+            model.date = DateTime.Now;
+            model.user = 1;
+            model.outputs = 0;
+            if (ModelState.IsValid) {
+                db.Inventory.Add(model);
+                db.SaveChanges();
+                ViewBag.message = "¡Registro guardado satisfactoriamente!";
+                ViewBag.messageType = "success";
+            } else {
+                ViewBag.message = "El registro no pudo ser guardado.";
+                ViewBag.messageType = "warning";
+            }
+
+            var stock = db.Inventory.ToList();
+            LoadProducts();
+            return View("Inventory", stock);
+        }
+
+        public IActionResult Update(int id) {
+            var stock = db.Inventory.Find(id);
+            LoadProducts();
+            return View(stock);
+        }
+
+        public IActionResult Delete(int id) {
+            var product = db.Inventory.Find(id);
+            db.Inventory.Remove(product);
+            db.SaveChanges();
+
+            ViewBag.message = "¡Registro eliminado satisfactoriamente!";
+            ViewBag.messageType = "success";
+
+            var stock = db.Inventory.ToList();
+            LoadProducts();
+            return View("Inventory", stock);
+        }
+
+        private void LoadProducts() {
+            var products = db.Products.ToList();
+            ViewBag.products = products;
+        }
+
+    }
+}
---
+++ Inventario/Inventario/Models/InventoryModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inventario.Models {
+
+    [Table("inventario")]
+    public class InventoryModel {
+        [Column("entradas")]
+        public int inputs { get; set; }
+
+        [Column("salidas")]
+        public int outputs { get; set; }
+
+        [Column("existencia")]
+        public int stock { get; set; }
+
+        [Column("fecha")]
+        public DateTime date { get; set; }
+
+        public int id { get; set; }
+
+        [Column("id_producto")]
+        public int product { get; set; }
+
+        [Column("id_usuario")]
+        public int user { get; set; }
+    }
+}
